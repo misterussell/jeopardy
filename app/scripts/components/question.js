62257a42aa9c33685f3792d27ee0167c25f48896
Maxwell Russell <maxruss.87@gmail.com>
2016-11-11 16:12:16 -0600

    breaking down components for click handling

--- app/scripts/components/question.js
+++ app/scripts/components/question.js
@@ -1,15 +1,46 @@
 import React from 'react';
 
+import ModalBlock from './modalBlock';
+
+import store from '../store';
+
 export default React.createClass({
+  getInitialState() {
+    return {
+      score: store.score,
+      modal: false
+    };
+  },
   render() {
+    const q = this.props.question;
+
+    //value handling
+    let value;
+    if(!q.get('answered')) {
+      value = q.get('value');
+    } else value = '';
+
+    //modal handling
+    let modalBlock;
+    if (this.state.modal) {
+      modalBlock = <ModalBlock data={q}/>;
+    } else modalBlock = '';
+
     return (
-      <li onClick={this.handleClick}>
-        {this.props.question.get('value')}
-      </li>
+      <main>
+        {modalBlock}
+        <li onClick={this.handleClick}>
+          {value}
+        </li>
+      </main>
     );
   },
   handleClick(e) {
     e.preventDefault();
-    console.log('clicked');
+    console.log(store);
+    console.log(store.categories);
+    let test = store.categories.get();
+    console.log(test);
+    test.get()
   }
 });
--- app/scripts/components/questionList.js
+++ app/scripts/components/questionList.js
@@ -1,40 +1,12 @@
 import React from 'react';
 
 import Question from './question';
-import ModalBlock from './modalBlock';
-
-import store from '../store';
 
 export default React.createClass({
-  getInitialState() {
-    return {
-      score: store.score,
-      modal: false
-    };
-  },
   render() {
-    let modalBlock;
-    if (this.state.modal) {
-      modalBlock = <ModalBlock />;
-    } else modalBlock = '';
     let questions = this.props.data.map((question, i, arr) => {
       return <Question key={question.get('id')} question={question} />
     });
-    return (
-      <main>
-        {modalBlock}
-        <ul>{questions}</ul>
-      </main>
-      );
-  },
-  handleClick(e) {
-    e.preventDefault();
-    //on click should render modal
-    this.setState({modal: true});
-    console.log('clicked');
-    console.log(e.target);
-
+    return <ul>{questions}</ul>;
   }
 });
-
-//change the state at the very top level if if the question is answered
